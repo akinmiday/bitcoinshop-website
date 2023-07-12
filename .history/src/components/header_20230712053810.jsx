c89b1bdba7cc7145eda2eb44cd49a28d10c3030a
akinmiday <110313060+akinmiday@users.noreply.github.com>
2023-07-12 07:31:15 +0100

    mobile responsiveness added

---
+++ .history/src/components/header_20230712053810.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import logoImg from "../assets/bitcoinshop-logo.png"
+import lightIcon from "../assets/Light-icon.png"
+
+
+export default function Header() {
+    return (<div className="header" >
+        <div className="left-side-header" ><img src={logoImg} />
+            <h3>Bitcoin<span className="shop-logo" >Shop</span></h3>
+        </div>
+        <div className="middle-section-header" >
+            <h4>Home</h4>
+            <h4>Contact us</h4>
+            <h4>About us</h4>
+            <h4>FAQs</h4>
+            <h4>Terms of Service</h4>
+        </div>
+        <div className="right-side-header" >
+            <img src={lightIcon} />
+            <button>Trade now</button>
+        </div>
+        <div className="menu-icon-header" >
+            <img />
+        </div>
+
+
+    </div>)
+}
